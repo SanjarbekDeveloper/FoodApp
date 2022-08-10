bab0a0608fdf8c019ccd1983373a8381829f7cb3
Пользователь <sanjarsatibaldiev61@gmail.com>
2022-08-10 10:37:40 +0500

    MyFirst project

---
+++ app/src/main/java/com/example/foodapp/IteamList.kt
@@ -0,0 +1,13 @@
+package com.example.foodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class IteamList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iteam_list)
+
+    }
+}
---
+++ app/src/main/java/com/example/foodapp/models/MyFood.kt
@@ -0,0 +1,7 @@
+package com.example.foodapp.models
+
+data class MyFood(
+    var name: String,
+    var kerakliMahsulotlar: String,
+    var tayyorlashTartibi: String
+)
